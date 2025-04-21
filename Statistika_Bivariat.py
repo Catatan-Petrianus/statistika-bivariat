20182b00d548b41e52e9297c2fa5bde0cf561ffd
Catatan-Petrianus <catatan.petrianus@gmail.com>
2025-04-22 06:53:50 +0700

    Add files via upload

---
+++ Statistika_Bivariat.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Configure page layout
+st.set_page_config(page_title="Regresi Linear")
+st.title("Regresi Linear")
+st.sidebar.success("Pilih Materi di atas.")
+
+st.markdown(
+    """
+    ### Materi yang dipelajari:
+    - ##### Diagram Pencar
+    - ##### Koefisien korelasi
+    - ##### Koefisien determinasi
+    - ##### Regresi Linear
+    """
+)
