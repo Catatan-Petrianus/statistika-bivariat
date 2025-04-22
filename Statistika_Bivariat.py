27095ea163e539669d49690c2096d802095d4f89
Catatan-Petrianus <catatan.petrianus@gmail.com>
2025-04-22 12:55:04 +0700

    Update Statistika_Bivariat.py

--- Statistika_Bivariat.py
+++ Statistika_Bivariat.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Configure page layout
 st.set_page_config(page_title="Regresi Linear")
 st.title("Regresi Linear")
-st.sidebar.success("Pilih Materi di atas.")
+st.sidebar.success("Pilih materi di atas.")
 
 st.markdown(
     """
