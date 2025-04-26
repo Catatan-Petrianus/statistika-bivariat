4b350f2e3e2cc372696c5ad1bf5daed8e2fdd00b
Catatan-Petrianus <catatan.petrianus@gmail.com>
2025-04-26 08:29:28 +0700

    Update 9_9️⃣_Mencari_persamaan_regresi_linear.py

--- pages/9_9️⃣_Mencari_persamaan_regresi_linear.py
+++ pages/9_9️⃣_Mencari_persamaan_regresi_linear.py
@@ -16,8 +16,8 @@ if 'df_rl' not in st.session_state:
         "y": np.random.randint(1, 11, 5)
     })
     
-tabel_baru_rl_btn = st.button('🎲 Buatkan tabel baru')    
-if tabel_baru_rl_ btn:
+  
+if st.button('🎲 Buatkan tabel baru'):
     st.session_state.step = 1
     st.session_state.df_rl = pd.DataFrame({
         "x": np.random.randint(1, 11, 5),
