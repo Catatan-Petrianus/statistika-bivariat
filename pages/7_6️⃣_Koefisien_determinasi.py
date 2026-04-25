3ae72cd7b80220f8d1eb30edf64b6f75c278e68f
Catatan-Petrianus <catatan.petrianus@gmail.com>
2026-04-25 20:22:53 +0700

    Update 7_6️⃣_Koefisien_determinasi.py

--- pages/7_6️⃣_Koefisien_determinasi.py
+++ pages/7_6️⃣_Koefisien_determinasi.py
@@ -58,7 +58,7 @@ def generate_data(correlation_type, size=100):
             r_squared = r_value ** 2
 
         # Check conditions
-        if correlation_type in ['Positif sangat kuat'] and 0.0.9 <= r_squared < 1:
+        if correlation_type in ['Positif sangat kuat'] and 0.9 <= r_squared < 1:
             return x, y, r_squared
         elif correlation_type in ['Positif kuat', 'Negatif kuat'] and 0.75 <= r_squared < 0.9:
             return x, y, r_squared
